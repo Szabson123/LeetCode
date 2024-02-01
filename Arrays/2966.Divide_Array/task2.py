ced80f9f42782b232ba072f7819ebbaa1d7af34b
Szabson123 <szymonzaba18@gmail.com>
2024-02-01 15:28:20 +0100

    added task2 what to do

--- Arrays/2966.Divide_Array/task2.py
+++ Arrays/2966.Divide_Array/task2.py
@@ -1,3 +1,11 @@
+# Dana jest tablica liczb całkowitych nums oraz liczba całkowita D. Twoim zadaniem jest podzielić tę tablicę na podciągi
+# (podtablice) w taki sposób, aby każdy element z nums znalazł się w dokładnie jednym podciągu, a maksymalna różnica między
+# jakimikolwiek dwoma elementami w każdym podciągu była mniejsza lub równa D. Każdy podciąg powinien zawierać co najmniej dwa elementy.
+# Warunki:
+# Każdy element z nums musi być użyty dokładnie raz.
+# Maksymalna różnica między dowolnymi dwoma elementami w podciągu nie może przekroczyć D.
+# Zwróć listę podciągów jako wynik. Jeśli istnieje wiele możliwych rozwiązań, zwróć dowolne z nich. Jeśli nie można utworzyć takich podciągów, zwróć pustą listę.
+
 def test():
     group = []
     nums = [4, 3, 2, 9, 1, 7]
