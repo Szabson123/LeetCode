4326d1a73a38ad8e25fdfd8cfb2bb2fa2274cf52
Szymon Żaba <szymonzaba18@gmail.com>
2024-02-07 23:17:26 +0100

    add exercise to(lambda) and (exercise)

---
+++ Strings/451Sort_ Character_By_Frequency/exercise(.join).py
@@ -0,0 +1,10 @@
+# Ćwiczenie 4: Użyj metody .join(), aby połączyć poniższą listę słów w jeden
+# ciąg tekstowy, gdzie każde słowo jest oddzielone przecinkiem. Lista: ["jabłko", "banan", "pomarańcza"].
+
+# Ćwiczenie 5: Stwórz ciąg tekstowy, w którym każda litera z poniższego
+# słowa jest oddzielona znakiem gwiazdki (*). Słowo: "python".
+
+# Ćwiczenie 6: Użyj metody .join() i wyrażenia lambda, aby połączyć liczby
+# z listy w jeden ciąg tekstowy, gdzie każda liczba jest oddzielona spacją.
+# Przed połączeniem konwertuj liczby na tekst. Lista: [1, 2, 3, 4, 5].
+
---
+++ Strings/451Sort_ Character_By_Frequency/exercise(lambda).py
@@ -0,0 +1,9 @@
+# Ćwiczenie 1: Napisz wyrażenie lambda, które przyjmuje jeden argument (liczbę) i zwraca jego kwadrat.
+
+# Ćwiczenie 2: Użyj wyrażenia lambda i funkcji filter(), aby wybrać z listy tylko te liczby,
+# które są parzyste. Przykładowa lista: [1, 2, 3, 4, 5, 6].
+
+# Ćwiczenie 3: Użyj wyrażenia lambda i funkcji sorted(),
+# aby posortować poniższą# listę krotek po drugim elemencie każdej krotki:
+# [(1, 2), (3, 1), (5, 0), (4, 2)].
+
