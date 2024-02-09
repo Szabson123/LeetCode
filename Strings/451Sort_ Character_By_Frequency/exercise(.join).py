adc4a0acd877440cdbed5929544ebde1c8ff11b1
Szymon Żaba <szymonzaba18@gmail.com>
2024-02-09 23:40:52 +0100

    done 2 tasks .join

--- Strings/451Sort_ Character_By_Frequency/exercise(.join).py
+++ Strings/451Sort_ Character_By_Frequency/exercise(.join).py
@@ -8,3 +8,24 @@
 # z listy w jeden ciąg tekstowy, gdzie każda liczba jest oddzielona spacją.
 # Przed połączeniem konwertuj liczby na tekst. Lista: [1, 2, 3, 4, 5].
 
+# ZAD1
+
+lista = ["jabłko", "banan", "pomarańcza"]
+
+new_list = ', '.join(lista)
+
+print(new_list)
+
+# ZAD2
+
+word = 'python'
+tekst = [tekst for tekst in word]
+
+done = '*'.join(tekst)
+print(done)
+# LUB
+
+word1 = 'python'
+done1 = '*'.join(word)
+
+print(done1)
